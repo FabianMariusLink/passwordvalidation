22c645e380f42571ca2a6642e443ed4ce743ed11
FabianMariusLink <fabian.marius.link@gmail.com>
2023-09-25 16:26:30 +0200

    Add generatePassword test and method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        int passwordLength = 12; // Ändere die Länge nach Bedarf
+        int passwordLength = 12;
         String securePassword = PasswordGenerator.generateSecurePassword(passwordLength);
         System.out.println("Generiertes sicheres Passwort: " + securePassword);
 
